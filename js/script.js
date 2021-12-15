66271da9ad7c823d8f56cc5cf8f9274cf681b9b5
Joungwoo Baik <joungwoobaik@Joungwoos-Air.attlocal.net>
2021-12-14 21:06:20 -0500

    Specific scroll animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
   let yOffSet = 0;
   let currentScene = 0;
   let prevScrollHeight = 0;
+  let enterNewScene = true;
 
   const scene_info = [
     {
@@ -10,6 +11,13 @@
       scrollHeight: 0,
       objs: {
         container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d"),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       },
     },
     {
@@ -45,33 +53,88 @@
         i
       ].objs.container.style.height = `${scene_info[i].scrollHeight}px`;
     }
+
+    let totalScrollHeight = 0;
+
+    for (let i = 0; i < scene_info[i]; i++) {
+      totalScrollHeight += scene_info.scrollHeight;
+      if (yOffSet <= totalScrollHeight) {
+        currentScene = i;
+        break;
+      }
+    }
+  };
+
+  const calcValues = (values, currentYOffset) => {
+    let rv;
+    const scrollHeight = scene_info[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    if (values.length === 3) {
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+    }
+    rv = parseFloat(scrollRatio * (values[1] - values[0]));
+
+    return rv;
+  };
+
+  const playAnimation = () => {
+    const currentObj = scene_info[currentScene].objs;
+    const currentYOffset = yOffSet - prevScrollHeight;
+    switch (currentScene) {
+      case 0:
+        let messageA_opacity_in = calcValues(
+          scene_info[currentScene].values.messageA_opacity,
+          currentYOffset
+        );
+
+        console.log("hi", messageA_opacity_in);
+        currentObj.messageA.style.opacity = messageA_opacity_in;
+
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+
+      default:
+        break;
+    }
   };
 
   const scrollLoop = () => {
     prevScrollHeight = 0;
+    enterNewScene = false;
+
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += scene_info[i].scrollHeight;
     }
 
     if (yOffSet > prevScrollHeight + scene_info[currentScene].scrollHeight) {
-      console.log("yes it worked");
       currentScene += 1;
+      enterNewScene = true;
     }
 
     if (yOffSet < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) {
         return;
       }
       currentScene -= 1;
     }
-
-    console.log("currentScene", currentScene);
+    if (enterNewScene) return;
+    playAnimation();
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
   };
 
-  window.addEventListener("resize", setLayout);
   window.addEventListener("scroll", () => {
     yOffSet = window.pageYOffset;
     scrollLoop();
   });
-  setLayout();
+  window.addEventListener("resize", setLayout);
+  window.addEventListener("load", setLayout);
 })();
